6e508ff3b55b9940ddd595d19dd885c4316c7dd2
TAK <tahir2in@gmail.com>
2021-03-18 02:29:04 +0400

    1 - fixed issues in addSolution method. 2 - added test cases for SolnSquareVerifier.

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -14,41 +14,48 @@ contract SolnSquareVerifier is DigiHouseToken {
 	// TODO define a solutions struct that can hold an index & an address
 	struct Solution {
 		uint256 index;
-		address solutioner;
+		address addr;
+		bool isSolved;
+		bool isUsed;
 	}
 
 	// TODO define an array of the above struct
-	Solution[] solutionArr;
+	mapping(uint256 => Solution) solutionMapping;
 
 	// TODO define a mapping to store unique solutions submitted
-	mapping(bytes32 => bool) submittedSolution;
+	mapping(bytes32 => bool) uniqueSolution;
 
 	// TODO Create an event to emit when a solution is added
-	event SolutionAdded(address solutioner);
+	event SolutionAdded(address solutioner, uint256 index);
 
 	// TODO Create a function to add the solutions to the array and emit the event
-	function _addSolution(uint256 index, address solAddress) private {
-		solutionArr.push(Solution(index, solAddress));
-		emit SolutionAdded(solAddress);
-	}
-
-	// TODO Create a function to mint new NFT only after the solution has been verified
-	//  - make sure the solution is unique (has not been used before)
-	//  - make sure you handle metadata as well as tokenSuplly
-	function mintNFT(
-		address solutionerAddress,
+	function addSolution(
+		address addr,
 		uint256 index,
 		uint256[2] memory a,
 		uint256[2][2] memory b,
 		uint256[2] memory c,
 		uint256[2] memory input
-	) public returns (bool) {
+	) public {
 		require(sqareVerifier.verifyTx(a, b, c, input), "Invalid solution");
 		bytes32 inputHash = keccak256(abi.encodePacked(a, b, c, input));
-		require(submittedSolution[inputHash] == false, "Solution already exists.");
-		submittedSolution[inputHash] = true;
-		_addSolution(index, solutionerAddress);
-		return mint(solutionerAddress, index);
+		require(uniqueSolution[inputHash] == false, "Solution already exists.");
+		uniqueSolution[inputHash] = true;
+		solutionMapping[index] = Solution(index, addr, true, false);
+		emit SolutionAdded(addr, index);
+	}
+
+	// TODO Create a function to mint new NFT only after the solution has been verified
+	//  - make sure the solution is unique (has not been used before)
+	//  - make sure you handle metadata as well as tokenSuplly
+	function mintNFT(address to, uint256 tokenId) public returns (bool) {
+		require(solutionMapping[tokenId].isSolved, "Solution is not verified");
+		require(solutionMapping[tokenId].isUsed == false, "Solution has been used before to mint token");
+		require(solutionMapping[tokenId].addr == to, "Verifier address is not same as minter");
+
+		bool temp = super.mint(to, tokenId);
+		solutionMapping[tokenId].isUsed = true;
+		return temp;
 	}
 
 	SqareVerifier sqareVerifier;
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,36 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
-
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+const Verifier = artifacts.require("Verifier");
+const { proof, inputs } = require("../../zokrates/code/square/proof");
+
+contract("TestSolnSquareVerifier", (accounts) => {
+  const account_one = accounts[0];
+  const account_two = accounts[1];
+
+  describe("SolnSquareVerifier", function () {
+    beforeEach(async function () {
+      let verifierContract = await Verifier.new({ from: account_one });
+      this.contract = await SolnSquareVerifier.new(verifierContract.address, { from: account_one });
+    });
+
+    // Test if a new solution can be added for contract - SolnSquareVerifier
+    it("can add new solution for contract - SolnSquareVerifier", async function () {
+      let tx = await this.contract.addSolution(account_one, 110, proof.a, proof.b, proof.c, inputs);
+      let eventLogged = tx.logs[0].event;
+      assert.equal(eventLogged, "SolutionAdded", "should evmit SolutionAdded Event");
+    });
+
+    // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+    it("can mint ERC721 token for contract - SolnSquareVerifier", async function () {
+      try {
+        await this.contract.addSolution(account_one, 110, proof.a, proof.b, proof.c, inputs);
+        let tx = await this.contract.mintNFT(account_one, 110);
+        // console.log(JSON.stringify(tx, null, 2))
+        let eventLogged = tx.logs[0].event;
+        assert.equal(eventLogged, "Transfer", "should emit Transfer Event");
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  });
+});
